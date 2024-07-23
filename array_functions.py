0fd6f758537accefc1c212ba122e16a168633918
Bharat Kesari <bkesari1998@gmail.com>
2024-07-22 23:54:20 -0400

    Wrote "deriv" function

--- array_functions.py
+++ array_functions.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy import ndarray
 
+from typing import Callable
+
 def square(x: ndarray) -> ndarray:
     '''
     Square each element in the input ndarray.
@@ -17,5 +19,15 @@ def leaky_relu(x: ndarray) -> ndarray:
 
     return np.maximum(0.2 * x, x)
 
+
+def deriv(func: Callable[[ndarray], ndarray],
+          input_: ndarray,
+          delta: float = 0.001 ) -> ndarray:
+    '''
+    Evaluates the derivative of a function "func" at every element in the
+    "input_" array.
+    '''
+
+    return ((func(input_ + delta) - func(input_)) / (2 * delta)) 
 
     
